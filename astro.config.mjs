c924244d55d2aaef0137af4e40b249fa03af850c
nick jones <n.jones@auckland.ac.nz>
2025-07-30 18:40:09 +1200

    proxy astro and tinacms onto the same port to enable tina admin overlay for content editing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,15 @@ import tinaDirective from "./astro-tina-directive/register"
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
-	integrations: [mdx(), sitemap(), react(), tinaDirective()],
+    // This is the new block to add for the dev server
+    server: {
+        proxy: {
+          '/admin': 'http://localhost:4001',
+          '/graphql': 'http://localhost:4001',
+        },
+    },
+
+    // Your existing site and integrations configuration
+    site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
+    integrations: [mdx(), sitemap(), react(), tinaDirective()],
 });
